97fc4dc7c57895939658e92ad3010f0434a8ae10
Alexandro Strack <alexandrostrack@gmail.com>
2017-12-31 03:31:01 -0200

    Main page to get name and email is done.

--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,11 @@
 const router = require('express').Router();
 
 router.get('/', function(req, res){
-    res.send('It works!');
+    res.render('index.ejs');
+});
+
+router.post('/inscrever', function(req,res){
+    //fazer a conexão com a API do mailchimp
 });
 
 module.exports = router;
